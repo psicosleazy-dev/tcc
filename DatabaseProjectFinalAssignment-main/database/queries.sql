fbc07d0bb429372fbfe9e6c9479989b56a3b31dc
Lucas Brum <lucasmatheuslegal@gmail.com>
2024-03-25 13:16:25 -0300

    organizando e gerando mais uma query uhull

--- DatabaseProjectFinalAssignment-main/database/queries.sql
+++ DatabaseProjectFinalAssignment-main/database/queries.sql
@@ -1,18 +1,36 @@
-SELECT ano, processo_seletivo, curso, nr_vagas FROM processo_seletivos_graduacao;
+-- SELECT ano, processo_seletivo, curso, nr_vagas FROM processo_seletivos_graduacao;
 
-SELECT nome_curso, SUM(evadidos) AS total_evadidos
-FROM quantitativo_alunos_graduacao
-GROUP_BY nome_curso
+SELECT nomecurso, SUM(evadidos) AS total_evadidos
+FROM quantitativo_alunos
+GROUP_BY nomecurso
 ORDER BY total_evadidos DESC
 LIMIT 5;
 
-SELECT nome_curso, ano, periodo, ((evadidos*100/vinculados)) AS pct_evadidos
-FROM quantitativo_alunos_graduacao
+SELECT nomecurso, ano, periodo, ((evadidos*100/vinculados)) AS pct_evadidos
+FROM quantitativo_alunos
 WHERE ((evadidos*100/vinculados)) < 100
 ORDER BY pct_evadidos DESC
 LIMIT 5;
 
-SELECT nome_curso,ano,periodo,sum(evadidos) AS evadidos
-FROM quantitativo_alunos_graduacao
+SELECT nomecurso,ano,periodo,sum(evadidos) AS evadidos
+FROM quantitativo_alunos
 GROUP BY nome_curso,ano,periodo
 ORDER BY ano,periodo;
+
+-- consultas novas
+
+SELECT ano, SUM(ingressantes) AS Total_Ingressantes, SUM(evadidos) AS Total_Evadidos
+FROM quantitativo_alunos
+GROUP BY ano;
+
+SELECT
+    ano,
+    SUM(evadidos) AS Total_Evadidos,
+    SUM(ingressantes) AS Total_Ingressantes,
+    SUM(evadidos) * 1.0 / SUM(ingressantes) AS Proporcao_Evadidos_Ingressantes
+FROM
+    quantitativo_alunos
+GROUP BY
+    Ano
+ORDER BY
+    Proporcao_Evadidos_Ingressantes DESC;
--- Script-4.sql
+++
@@ -1,3 +0,0 @@
-SELECT ano, SUM(ingressantes) AS Total_Ingressantes, SUM(evadidos) AS Total_Evadidos
-FROM quantitativo_alunos
-GROUP BY ano;
